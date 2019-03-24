78ed9f169ec6f572b3f9dfdcf357db9d31290566
LAP <2300820209@qq.com>
2019-03-24 14:30:21 +0800

    maxProfit

--- leetCode/maxProfit.js
+++ leetCode/maxProfit.js
@@ -3,12 +3,23 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let min = Math.min.apply(Math,prices);
-    let max = Math.max.apply(Math,prices);
-    if(prices.indexOf(min) < prices.indexOf(max)){
-       return max - min;
-       }else{
-           return 0;
-       }
-    
+    if(prices.length === 0 || prices.length === 1){
+       return 0;
+    }
+    let used = [];
+    used[0] = prices[0];
+    used[1] = prices[1];
+    let max = used[1] - used[0];
+    let min;
+    for(let i = 2, length = prices.length; i < length; i++){
+        min = Math.min.apply(Math,used);
+        if((prices[i] - min) > max){
+            max = prices[i] - min;
+        }
+        used.push(prices[i]);
+    }
+    if(max < 0){
+       return 0;
+    }
+    return max;
 };
